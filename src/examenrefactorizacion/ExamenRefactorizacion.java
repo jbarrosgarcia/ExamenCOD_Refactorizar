b7f25ce81aebb766398484b19fc4b1f2191e925c
jbarrosgarcia <jbarrosgarcia@atanasoff07.danielcastelao.org>
2015-12-10 08:53:18 +0100

    Renombre las variables

--- src/examenrefactorizacion/ExamenRefactorizacion.java
+++ src/examenrefactorizacion/ExamenRefactorizacion.java
@@ -7,9 +7,9 @@ public class ExamenRefactorizacion {
     public static void main(String[] args) {
         int totalDigitos = 3;
         int numDigitos = 0;
-        
-        for (int i = 1; i <= 99999; i++) {
-            int aux = i;
+            introducirDigitos(totalDigitos);
+        for (int numeroComparar = 1; numeroComparar <= 99999; numeroComparar++) {
+            int aux = numeroComparar;
 
             int contador = 0;
 
@@ -20,21 +20,21 @@ public class ExamenRefactorizacion {
             numDigitos = contador;
 
             if (numDigitos == totalDigitos) {
-                if (i < 4) {
+                if (numeroComparar < 4) {
                     esPrimo = true;
                 } else {
-                    if (i % 2 == 0) {
+                    if (numeroComparar % 2 == 0) {
                         esPrimo = false;
                     } else {
                         int contador1 = 0;
                         int i1 = 1;
-                        int k = (i - 1) / 2;
+                        int k = (numeroComparar - 1) / 2;
                         if (k % 2 == 0) {
                             k--;
                         }
 
                         while (i1 <= k) {
-                            if (i % i1 == 0) {
+                            if (numeroComparar % i1 == 0) {
                                 contador1++;
                             }
                             i1 += 2;
@@ -50,16 +50,17 @@ public class ExamenRefactorizacion {
                 }
 
                 if (esPrimo == true) {
-                    System.out.println(i);
+                    System.out.println(numeroComparar);
                 }
             }
         }
     }
-//    public void introducirDigitos(int totalDigitos){
+//    public static int introducirDigitos(int totalDigitos){
 //        Scanner aux=new Scanner(System.in);
 //        if (totalDigitos <= 0) {
 //            System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
 //            totalDigitos=aux.nextInt();
 //        }
+//        return totalDigitos;
 //    }
 //}
