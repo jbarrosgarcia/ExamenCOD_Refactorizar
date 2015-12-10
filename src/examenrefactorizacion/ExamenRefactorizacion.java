6604ad0842a94c710959a3becac9b5a1e27999ff
jbarrosgarcia <jbarrosgarcia@atanasoff07.danielcastelao.org>
2015-12-10 09:15:19 +0100

    Renombre las variables y añadi los metodos que me faltaban

--- src/examenrefactorizacion/ExamenRefactorizacion.java
+++ src/examenrefactorizacion/ExamenRefactorizacion.java
@@ -1,24 +1,22 @@
 package examenrefactorizacion;
+
 import java.util.Scanner;
+
 public class ExamenRefactorizacion {
 
     public static boolean esPrimo = false;
+    public static int totalDigitos = 3;
 
     public static void main(String[] args) {
-        int totalDigitos = 3;
+
         int numDigitos = 0;
-            introducirDigitos(totalDigitos);
+        introducirDigitos();
         for (int numeroComparar = 1; numeroComparar <= 99999; numeroComparar++) {
-            int aux = numeroComparar;
+            int divisionEntera = numeroComparar;
 
             int contador = 0;
-
-            while (aux != 0) {
-                aux = aux / 10;
-                contador++;
-            }
-            numDigitos = contador;
-
+            contadorDivisionEntera(numeroComparar);
+           
             if (numDigitos == totalDigitos) {
                 if (numeroComparar < 4) {
                     esPrimo = true;
@@ -26,26 +24,13 @@ public class ExamenRefactorizacion {
                     if (numeroComparar % 2 == 0) {
                         esPrimo = false;
                     } else {
-                        int contador1 = 0;
+                        int contadorDivisiones = 0;
                         int i1 = 1;
-                        int k = (numeroComparar - 1) / 2;
-                        if (k % 2 == 0) {
-                            k--;
+                        int limite = (numeroComparar - 1) / 2;
+                        if (limite % 2 == 0) {
+                            limite--;
                         }
-
-                        while (i1 <= k) {
-                            if (numeroComparar % i1 == 0) {
-                                contador1++;
-                            }
-                            i1 += 2;
-                            if (contador1 == 2) {
-                                i1 = k + 1;
-                            }
-                        }
-
-                        if (contador1 == 1) {
-                            esPrimo = true;
-                        }
+                        esPrimo = calculaNumDivisores(i1, limite, numeroComparar, contadorDivisiones);
                     }
                 }
 
@@ -55,12 +40,48 @@ public class ExamenRefactorizacion {
             }
         }
     }
-//    public static int introducirDigitos(int totalDigitos){
-//        Scanner aux=new Scanner(System.in);
-//        if (totalDigitos <= 0) {
-//            System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
-//            totalDigitos=aux.nextInt();
-//        }
-//        return totalDigitos;
-//    }
-//}
+     public static boolean calculaNumDivisores(int i1, int limite, int numComparar, int contadorDivisores) {
+        boolean esPrimo = false;
+        while (i1 <= limite) {
+            if (numComparar % i1 == 0) {
+                contadorDivisores++;
+            }
+            i1 += 2;
+            if (contadorDivisores == 2) {
+                i1 = limite + 1;
+            }
+        }
+        if (contadorDivisores == 1) {
+            esPrimo = true;
+        }
+        return esPrimo;
+    }
+
+    public static int introducirDigitos() {
+        Scanner aux = new Scanner(System.in);
+        System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
+        if (totalDigitos <= 0) {
+
+            totalDigitos = aux.nextInt();
+        } else {
+            if (totalDigitos <= 0) {
+                System.out.println("Ingreso un numero erroneo por favor\nIngrese como parámetro, un numero de digitos correcto (mayor que 0): ");
+                totalDigitos = aux.nextInt();
+            }
+        }
+
+        return totalDigitos;
+    }
+      public static int contadorDivisionEntera(int numComparar) {
+        int ndigitos;
+        //cambia i por numcomparar.
+        int divisionEntera = numComparar;
+        int contador = 0;
+        while (divisionEntera != 0) {
+            divisionEntera = divisionEntera / 10;
+            contador++;
+        }
+        ndigitos = contador;
+        return ndigitos;
+    }
+}
